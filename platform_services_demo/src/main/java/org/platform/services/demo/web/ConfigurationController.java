d980f9e96b5e71e437e8c49eb549135840b4af16
A491382 <371937605@qq.com>
2017-04-08 20:57:50 +0800

    test

---
+++ platform_services_demo/src/main/java/org/platform/services/demo/web/ConfigurationController.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.platform.services.demo.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Brook
+ *
+ */
+@Controller
+public class ConfigurationController {
+	
+	@Value("${hello}")
+	private String hello;
+	
+	@RequestMapping(value = "demo/cfg", method = RequestMethod.GET)
+	@ResponseBody
+	public String cfg() {
+		return "hello "+hello;
+	}
+
+}
